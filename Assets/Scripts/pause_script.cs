1f793051ed977d360314f5abd79ad7a9930912b0
Manpreet993 <53800417+Manpreet993@users.noreply.github.com>
2019-10-29 13:37:54 +1030

    please cancel the pull request
    By mistake

---
+++ Assets/Scripts/pause_script.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pause_script : MonoBehaviour
+{
+    bool ispaused;
+    public GameObject PanelUI;
+    void Start()
+    {
+        PanelUI.SetActive(false);
+    }
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            Time.timeScale = 0;
+            ispaused = true;
+
+            PanelUI.SetActive(!PanelUI.activeSelf);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            ispaused = false;
+        }
+    }
+}
