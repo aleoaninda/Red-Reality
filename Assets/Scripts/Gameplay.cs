acd39b6bb596144e13083a8e978c7c7cafb2a787
Manpreet993 <53800417+Manpreet993@users.noreply.github.com>
2019-10-29 14:34:25 +1030

    Play menu

---
+++ Assets/Scripts/Gameplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour
+{
+    public GameObject MenuUI;
+    // Start is called before the first frame update
+    bool play;
+
+    public void SwitchShowHide()
+    {
+        play = !play;
+        MenuUI.gameObject.SetActive(play);
+    }
+}
