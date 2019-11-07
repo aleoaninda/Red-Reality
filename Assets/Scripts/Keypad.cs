ebaecadd2e74fc087ca08b19a1d108ce88e440c2
lin0287 <37977095+lin0287@users.noreply.github.com>
2019-11-07 15:18:52 +1030

    Update Keypad.cs
    Proximity check for keypad. If the player is too far away, keypad deactivates.

--- Assets/Scripts/Keypad.cs
+++ Assets/Scripts/Keypad.cs
@@ -15,11 +15,15 @@ public class Keypad : MonoBehaviour
 
     void Update()
 	{
-        if (Mathf.Abs(player.transform.localPosition.y) < 2.5 && Mathf.Abs(player.transform.localPosition.x) < 2.5) { 
-		    if (Input.GetButtonDown("Keypad"))
+        if (Mathf.Abs(player.transform.localPosition.y) < 2.5 && Mathf.Abs(player.transform.localPosition.x) < 2.5) { //If the player is close to the keypad (Tom)
+		    if (Input.GetButtonDown("Keypad")) //if Keypad button is pressed down
 		    {
 		    	keypadUI.SetActive(!keypadUI.activeSelf);
 		    }
+        }
+        else if (!(Mathf.Abs(player.transform.localPosition.y) < 2.5 && Mathf.Abs(player.transform.localPosition.x) < 2.5) && keypadUI.active == true) //If the player is not close to keypad and it's active
+        {
+            keypadUI.SetActive(!keypadUI.activeSelf);
         }
     }
 }
